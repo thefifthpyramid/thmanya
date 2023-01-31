27ce1f16d9045f866236f8c499d104d02532e271
Ahmed Ali Klay <ahmedaliklay@outlook.com>
2023-01-31 21:04:19 +0200

    profile page :create article page

--- assets/js/script.js
+++ assets/js/script.js
@@ -237,3 +237,25 @@ if(window.innerWidth < 990){
 if(window.innerWidth < 570){
     $('.main-navbar .input-box').removeClass('open');
 }
+
+
+tinymce.init({
+    selector: '#article-textarea',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    skin: "oxide-dark",
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ]
+});
+
+
+// if($("#darken-stylesheet").attr("href") == "assets/css/dark.css") {
+//     tinymce.init({
+//         selector: '#article-textarea',
+//         skin: "oxide-dark",
+//     });
+// }
